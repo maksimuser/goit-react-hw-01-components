1438fc688355783354f2adbf055af24d3ba472d1
Dema Maksim <maksimdema@gmail.com>
2021-03-23 18:16:12 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,6 @@ import friends from './components/FriendList/friends.json';
 import statisticalData from './components/Statistics/statistical-data.json';
 import user from './components/Profile/user.json';
 
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
-
 const App = () => {
   return (
     <div>
